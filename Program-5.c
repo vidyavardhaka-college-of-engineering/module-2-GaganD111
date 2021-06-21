5134ca063081e3553313312f47c5d3c1e3674ca1
GaganD111 <84520742+GaganD111@users.noreply.github.com>
2021-06-21 16:01:07 +0530

    Done

--- Program-5.c
+++ Program-5.c
@@ -3,12 +3,11 @@
 
 
 #include <stdio.h>
- 
-#include <stdio.h>
+
  
 int main()
 {
-   int a[10],i,sum;
+   int a[5],i,sum,n=0;
    printf("Enter number of elements in array and the array elements");
    scanf("%d", &n);
  
@@ -17,9 +16,9 @@ int main()
    for (i = 0; i < n; i++)
    {
       scanf("%d", &a[i]);
-      sum=sum+a[]; 
+      sum=sum+a[i]; 
    }
- printf("Sum of elements=%d", );
+ printf("Sum of elements=%d",sum );
  
   return 0;
 }
